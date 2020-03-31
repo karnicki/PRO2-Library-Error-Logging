57dc4cda679fdaf0630c00af2886fa8ac93b8477
karnicki <s17129@pjwstk.edu.pl>
2020-03-31 22:17:04 +0200

    Initial Commit
    Example of error logging with NLog and Rollbar.

---
+++ Api/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Library.Models.DTO;
+using Library.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Rollbar;
+
+namespace Library.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(IUserRepository userRepository, ILogger<UsersController> logger)
+        {
+            _logger = logger;
+            _userRepository = userRepository;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            RollbarLocator.RollbarInstance.Error(new Exception("Wystąpił błąd z Rollbar ---TEST---"));
+            _logger.LogError("Wystąpił błąd");
+            
+            var res = await _userRepository.GetUsers();
+            return Ok(res);
+        }
+
+        [HttpPost(Name = nameof(AddUser))]
+        public async Task<IActionResult> AddUser([FromBody] UserDto user)
+        {
+           var res = await _userRepository.AddUser(user);
+
+            return CreatedAtRoute(nameof(AddUser), res);
+        }
+
+        [HttpGet("{idUser:int}")]
+        public async Task<IActionResult> GetUser([FromRoute] int idUser)
+        {
+            var res = await _userRepository.GetUser(idUser);
+            return Ok(res);
+        }
+        
+    }
+}
